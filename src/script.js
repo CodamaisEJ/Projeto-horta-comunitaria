bdaa25d6217f1e483394e8bc710c31d2d8ee55ca
Camila Carvalho <75643855+camilacarvalhon@users.noreply.github.com>
2022-03-18 17:05:37 -0300

    update arquivos

---
+++ src/script.js
@@ -0,0 +1,19 @@
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    autoWidth:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        700:{
+            items:2
+        },
+        900:{
+            items:3
+        }
+    }
+})
